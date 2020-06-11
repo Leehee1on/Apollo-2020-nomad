68a293a21c764cd04da7bedbfef4e1eb6c5b3ea3
admin <admin@adminui-MacBook-Pro.local>
2020-06-11 19:32:23 +0900

    1.3

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,81 @@
-export default () => "Detail"
+import React from "react"
+import { useParams } from "react-router-dom"
+import { gql } from "apollo-boost"
+import { useQuery } from "@apollo/react-hooks"
+import styled from "styled-components"
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`
+
+const Column = styled.div`
+  margin-left: 10px;
+`
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`
+
+const Description = styled.p`
+  font-size: 28px;
+`
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+`
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      title
+      medium_cover_image
+      language
+      rating
+      description_intro
+    }
+  }
+`
+export default () => {
+  let { id } = useParams()
+  id = Number(id)
+  const { loading, data } = useQuery(GET_MOVIE, { variables: { id } })
+
+  return (
+    <>
+      <Container>
+        <Column>
+          {}
+          <Title>{loading ? "loading.." : data.movie.title}</Title>
+          {!loading && data.movie && (
+            <>
+              <Subtitle>{data.movie.language} </Subtitle>
+              <Description>{data.movie.description_intro}</Description>
+            </>
+          )}
+        </Column>
+        <Poster></Poster>
+      </Container>
+      {/* {loading && <>Loading</>}
+      {!loading && data.movie && (
+        <>
+          {data.movie.title}
+          <img src={data.movie.medium_cover_image} />
+        </>
+      )} */}
+    </>
+  )
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,58 @@
 import React from "react"
 import { gql } from "apollo-boost"
 import { useQuery } from "@apollo/react-hooks"
-
+import styled from "styled-components"
+import Movie from "../components/Movie"
 // 여기다 graphql query문 작성
+
+const Container = styled.div`
+  display: flex;
+  display: flex;
+  flex-direction: column;
+  flex-direction: column;
+  align-items: center;
+  align-items: center;
+  width: 100%;
+  width: 100%;
+`
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`
+const Subtitle = styled.h3`
+  font-size: 35px;
+`
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`
 const GET_MOVIES = gql`
   {
     movies {
       id
       medium_cover_image
+      title
     }
   }
 `
@@ -15,11 +60,27 @@ const GET_MOVIES = gql`
 export default () => {
   const { loading, data } = useQuery(GET_MOVIES)
   console.log(loading, data)
-  if (loading) {
-    return "loading..."
-  }
-  if (data && data.movies) {
-    return data.movies.map((m) => <h1>{m.id}</h1>)
-  }
-  return <>Home</>
+  // if (loading) {
+  //   return "loading..."
+  // }
+  // if (data && data.movies) {
+  //   return data.movies.map((m) => (
+  //     <>
+  //       <h1>{m.id}</h1>
+  //       <img src={m.medium_cover_image} />
+  //     </>
+  //   ))
+  // }
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>test</Subtitle>
+      </Header>
+      {loading && <Loading>Loading</Loading>}
+      <Movies>
+        {!loading && data.movies && data.movies.map((m) => <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />)}
+      </Movies>
+    </Container>
+  )
 }
